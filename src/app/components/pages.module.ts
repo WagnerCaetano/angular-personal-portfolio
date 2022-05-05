8dbf879f9bf4f4d1f010fd2302e37c27aaedb490
Wagner Caetano <wagnercaetano@gmail.com>
2022-05-04 23:19:54 -0300

    [04/05] adjustments nav and starting footer

--- src/app/components/pages.module.ts
+++ src/app/components/pages.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavHeaderComponent],
-  exports: [NavHeaderComponent],
+  declarations: [NavHeaderComponent, FooterComponent],
+  exports: [NavHeaderComponent, FooterComponent],
 })
 export class ComponentsModule {}
